5c7568b055d448e770bf610ae7354fcc25504379
Davert <davert.php@resend.cc>
2014-02-23 01:32:17 +0200

    added composer install

--- RoboFile.php
+++ RoboFile.php
@@ -8,18 +8,20 @@ class Robofile extends \Robo\Tasks
         // ask for changes in this release
         $changelog = $this->taskChangelog()
             ->version($version)
-            ->askForChanges();
+            ->askForChanges()
+            ->run();
 
         // adding changelog and pushing it
-        $this->taskExec('git add CHANGELOG.md')->run();
-        $this->taskExec('git commit -m "updated changelog"')->run();
-        $this->taskExec('git push')->run();
+        $this->taskGit()
+            ->add('CHANGELOG.md')
+            ->commit('updated changelog')
+            ->push()
+            ->run();
 
         // create GitHub release
         $this->taskGitHubRelease($version)
             ->uri('Codeception/Specify')
             ->askDescription()
-            ->changes($changelog->getChanges())
             ->run();
     }
 
