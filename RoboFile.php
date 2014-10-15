953b7d92508e1b9473b3c05dc663cecffb3e2ef0
Davert <davert.php@resend.cc>
2014-10-15 05:57:29 +0300

    generated docs for configuration classes

--- RoboFile.php
+++ RoboFile.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
 
 class Robofile extends \Robo\Tasks
 {
@@ -28,6 +29,22 @@ class Robofile extends \Robo\Tasks
             ->version(file_get_contents('VERSION'))
             ->change($description)
             ->run();
+    }
+
+    protected $docs = [
+        'docs/GlobalConfig.md' => \Codeception\Specify\Config::class,
+        'docs/LocalConfig.md' => \Codeception\Specify\ConfigBuilder::class,
+    ];
+
+    public function docs()
+    {
+        foreach ($this->docs as $file => $class) {
+            class_exists($class, true);
+            $this->taskGenDoc($file)
+                ->docClass($class)
+                ->processProperty(false)
+                ->run();
+        }
     }
 
 
